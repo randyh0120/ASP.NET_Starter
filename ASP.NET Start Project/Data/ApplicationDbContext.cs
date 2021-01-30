6d5426301062609707919b572651e659e86abe24
Randy Hattab <46980727+randyh0120@users.noreply.github.com>
2021-01-29 21:15:48 -0500

    Updated

---
+++ ASP.NET Start Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASP.NET_Start_Project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
